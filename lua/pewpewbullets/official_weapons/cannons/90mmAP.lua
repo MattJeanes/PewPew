dce8b3120a473ac5f4dae8786f04779cd91776a2
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-08-03 16:20:35 -0600

    Update 90mmAP.lua

--- lua/pewpewbullets/official_weapons/cannons/90mmAP.lua
+++ lua/pewpewbullets/official_weapons/cannons/90mmAP.lua
@@ -6,7 +6,7 @@ local BULLET = {}
 BULLET.Version = 2
 
 -- General Information
-BULLET.Name = "90mm Cannon (HE)"
+BULLET.Name = "90mm Cannon (AP)"
 BULLET.Author = "Divran"
 BULLET.Description = "Aim away from face."
 BULLET.AdminOnly = false
