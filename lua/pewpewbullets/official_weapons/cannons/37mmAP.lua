9dc0030da98c50fbcfff7d83205625c2a87a458b
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-08-03 16:24:25 -0600

    Create 37mm AP
    Adds 37mm AP variant.

---
+++ lua/pewpewbullets/official_weapons/cannons/37mmAP.lua
@@ -0,0 +1,51 @@
+-- Basic Cannon
+
+local BULLET = {}
+
+-- Important Information
+BULLET.Version = 2
+
+-- General Information
+BULLET.Name = "37mm Cannon (AP)"
+BULLET.Author = "Divran"
+BULLET.Description = "Rapid fire, low damage."
+BULLET.AdminOnly = false
+BULLET.SuperAdminOnly = false
+
+-- Appearance
+BULLET.Model = "models/combatmodels/tankshell.mdl"
+BULLET.Material = nil
+BULLET.Color = nil
+BULLET.Trail = nil
+
+-- Effects / Sounds
+BULLET.FireSound = {"arty/37mm.wav"}
+BULLET.ExplosionSound = {"weapons/explode1.wav","weapons/explode2.wav"}
+BULLET.FireEffect = "cannon_flare"
+BULLET.ExplosionEffect = "HEATsplode"
+
+-- Movement
+BULLET.Speed = 175
+--BULLET.Gravity = 0.1
+BULLET.RecoilForce = 100
+BULLET.Spread = 0
+BULLET.AffectedBySBGravity = true
+
+-- Damage
+BULLET.DamageType = "BlastDamage"
+BULLET.Damage = 300
+BULLET.Radius = 10
+BULLET.RangeDamageMul = 2.2
+BULLET.NumberOfSlices = nil
+BULLET.SliceDistance = nil
+BULLET.PlayerDamage = 200
+BULLET.PlayerDamageRadius = 10
+
+-- Reloading/Ammo
+BULLET.Reloadtime = 3
+BULLET.Ammo = 0
+BULLET.AmmoReloadtime = 0
+
+BULLET.EnergyPerShot = 2500
+
+pewpew:AddWeapon( BULLET )
