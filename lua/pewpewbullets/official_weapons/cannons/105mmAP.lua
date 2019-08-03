3b4a0c3e250748a4d128784fd6535dee2911aebb
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-08-03 16:12:52 -0600

    create 105mm AP.
    Adds 105mm AP cannon.

---
+++ lua/pewpewbullets/official_weapons/cannons/105mmAP.lua
@@ -0,0 +1,51 @@
+-- Basic Cannon
+
+local BULLET = {}
+
+-- Important Information
+BULLET.Version = 2
+
+-- General Information
+BULLET.Name = "105mm Cannon (AP)"
+BULLET.Author = "Divran"
+BULLET.Description = "Slow rate of fire, high damage."
+BULLET.AdminOnly = false
+BULLET.SuperAdminOnly = false
+
+-- Appearance
+BULLET.Model = "models/combatmodels/tankshell_120mm.mdl"
+BULLET.Material = nil
+BULLET.Color = nil
+BULLET.Trail = nil
+
+-- Effects / Sounds
+BULLET.FireSound = {"arty/105mm.wav"}
+BULLET.ExplosionSound = {"weapons/explode1.wav","weapons/explode2.wav"}
+BULLET.FireEffect = "cannon_flare"
+BULLET.ExplosionEffect = "HEATsplode"
+
+-- Movement
+BULLET.Speed = 145
+--BULLET.Gravity = 0.1
+BULLET.RecoilForce = 500
+BULLET.Spread = 0
+BULLET.AffectedBySBGravity = true
+
+-- Damage
+BULLET.DamageType = "BlastDamage" -- Look in gcombat_damagecontrol.lua for available damage types
+BULLET.Damage = 800
+BULLET.Radius = 100
+BULLET.RangeDamageMul = 2.6
+BULLET.NumberOfSlices = nil
+BULLET.SliceDistance = nil
+BULLET.PlayerDamage = 200
+BULLET.PlayerDamageRadius = 50
+
+-- Reloading/Ammo
+BULLET.Reloadtime = 8
+BULLET.Ammo = 0
+BULLET.AmmoReloadtime = 0
+
+BULLET.EnergyPerShot = 9500
+
+pewpew:AddWeapon( BULLET )
