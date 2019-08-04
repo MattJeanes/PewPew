adfd3c16f7f03f91cab0f8fac3feb23d2bfd1e38
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-08-04 16:29:55 -0600

    Update AL105mmAP.lua

--- lua/pewpewbullets/official_weapons/cannons/AL105mmAP.lua
+++ lua/pewpewbullets/official_weapons/cannons/AL105mmAP.lua
@@ -6,7 +6,7 @@ local BULLET = {}
 BULLET.Version = 2
 
 -- General Information
-BULLET.Name = "105mm AL Cannon (AP)"
+BULLET.Name = "105mm Auto loading Cannon (AP)"
 BULLET.Author = "Divran"
 BULLET.Description = "105mm Auto loading Cannon."
 BULLET.AdminOnly = false
