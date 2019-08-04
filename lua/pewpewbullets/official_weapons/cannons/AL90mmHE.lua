a05f747d85d9746fd8318a3067889209aed5ec5a
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-08-03 20:50:29 -0600

    Create AL90mmHE.lua

---
+++ lua/pewpewbullets/official_weapons/cannons/AL90mmHE.lua
@@ -0,0 +1,51 @@
+-- Basic Cannon
+
+local BULLET = {}
+
+-- Important Information
+BULLET.Version = 2
+
+-- General Information
+BULLET.Name = "90mm Auto Loading Cannon (HE)"
+BULLET.Author = "Divran"
+BULLET.Description = "Aim away from face."
+BULLET.AdminOnly = false
+BULLET.SuperAdminOnly = false
+
+-- Appearance
+BULLET.Model = "models/combatmodels/tankshell.mdl"
+BULLET.Material = nil
+BULLET.Color = nil
+BULLET.Trail = nil
+
+-- Effects / Sounds
+BULLET.FireSound = {"arty/37mm.wav"}
+BULLET.ExplosionSound = {"weapons/explode1.wav","weapons/explode2.wav"}
+BULLET.FireEffect = "cannon_flare"
+BULLET.ExplosionEffect = "big_splosion"
+
+-- Movement
+BULLET.Speed = 100
+--BULLET.Gravity = 0.5
+BULLET.RecoilForce = 500
+BULLET.Spread = 0
+BULLET.AffectedBySBGravity = true
+
+-- Damage
+BULLET.DamageType = "BlastDamage"
+BULLET.Damage = 300
+BULLET.Radius = 400
+BULLET.RangeDamageMul = 2.2
+BULLET.NumberOfSlices = nil
+BULLET.SliceDistance = nil
+BULLET.PlayerDamage = 110
+BULLET.PlayerDamageRadius = 400
+
+-- Reloading/Ammo
+BULLET.Reloadtime = 3
+BULLET.Ammo = 5
+BULLET.AmmoReloadtime = 30
+
+BULLET.EnergyPerShot = 3700
+
+pewpew:AddWeapon( BULLET )
