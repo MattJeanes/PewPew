de9c4ec80c629d5bae84b7d154cc8f88403dda64
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-08-05 15:46:27 -0600

    Update 12.7mm.lua

--- lua/pewpewbullets/official_weapons/machineguns/12.7mm.lua
+++ lua/pewpewbullets/official_weapons/machineguns/12.7mm.lua
@@ -32,7 +32,7 @@ BULLET.AffectedBySBGravity = true
 
 -- Damage
 BULLET.DamageType = "PointDamage"
-BULLET.Damage = 25
+BULLET.Damage = 30
 BULLET.Radius = nil
 BULLET.RangeDamageMul = nil
 BULLET.NumberOfSlices = nil
