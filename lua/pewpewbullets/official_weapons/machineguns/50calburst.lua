d5edff534c11d6766537f635d3d5bbf6f7e92253
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-08-04 18:14:28 -0600

    Create 50calburst.lua

---
+++ lua/pewpewbullets/official_weapons/machineguns/50calburst.lua
@@ -0,0 +1,50 @@
+local BULLET = {}
+
+-- Important Information
+BULLET.Version = 2
+
+-- General Information
+BULLET.Name = "50 cal burst machinegun"
+BULLET.Author = "Hexwolf (Base by Divran)"
+BULLET.Description = "50 caliber burstfire machinegun."
+BULLET.AdminOnly = false
+BULLET.SuperAdminOnly = false
+
+-- Appearance
+BULLET.Model = "models/combatmodels/tankshell_25mm.mdl"
+BULLET.Material = nil
+BULLET.Color = nil
+BULLET.Trail = nil
+
+-- Effects / Sounds
+BULLET.FireSound = {"arty/50cal.wav"}
+BULLET.ExplosionSound = nil
+BULLET.FireEffect = "muzzleflash"
+BULLET.ExplosionEffect = "mghit"
+BULLET.EmptyMagSound = {"weapons/shotgun/shotgun_empty.wav"}
+
+-- Movement
+BULLET.Speed = 100
+--BULLET.Gravity = 0.15
+BULLET.RecoilForce = 50
+BULLET.Spread = 0.4
+BULLET.AffectedBySBGravity = true
+
+-- Damage
+BULLET.DamageType = "PointDamage"
+BULLET.Damage = 25
+BULLET.Radius = nil
+BULLET.RangeDamageMul = nil
+BULLET.NumberOfSlices = nil
+BULLET.SliceDistance = nil
+BULLET.PlayerDamage = nil
+BULLET.PlayerDamageRadius = nil
+
+-- Reloading/Ammo
+BULLET.Reloadtime = 0.2
+BULLET.Ammo = 3
+BULLET.AmmoReloadtime = 1
+
+BULLET.EnergyPerShot = 300
+
+pewpew:AddWeapon( BULLET )
