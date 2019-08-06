a0ab2cf9c486841a796f1fb93db828df494bc36d
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-08-06 17:41:50 -0600

    Update and rename plasma_cannon.lua to ppc.lua

--- lua/pewpewbullets/official_weapons/cannons/ppc.lua
+++ lua/pewpewbullets/official_weapons/cannons/ppc.lua
@@ -6,45 +6,46 @@ local BULLET = {}
 BULLET.Version = 2
 
 -- General Information
-BULLET.Name = "Plasma Cannon"
-BULLET.Author = "Colonel Thirty Two"
-BULLET.Description = "Shoots balls of plasma at people"
+BULLET.Name = "Particle Projection Cannon "
+BULLET.Author = "Hexwolf (Base by Colonel Thirty Two)"
+BULLET.Description = "Fires a condensed ball of superheated particles, causes electronics to briefly malfunction."
 BULLET.AdminOnly = false
 BULLET.SuperAdminOnly = false
 
 -- Appearance
 BULLET.Model = "models/combatmodels/tankshell_230mm.mdl"
 BULLET.Material = nil
-BULLET.Color = Color(150,150,255,255)
+BULLET.Color = Color(0,191,255,255)
 BULLET.Trail = { StartSize = 10,
 				 EndSize = 2,
-				 Length = 0.6,
+				 Length = 4,
 				 Texture = "trails/smoke.vmt",
-				 Color = Color( 150, 150, 255, 255 ) }
+				 Color = Color( 0,191,255, 255 ) }
 
 -- Effects / Sounds
 BULLET.FireSound = {"col32/bomb3.wav"}
 BULLET.ExplosionSound = {"weapons/explode1.wav","weapons/explode2.wav"}
 BULLET.FireEffect = "muzzleflash"
-BULLET.ExplosionEffect = "big_splosion"
+BULLET.ExplosionEffect = "HEATsplode"
 
 -- Movement
 BULLET.Speed = 50
 BULLET.RecoilForce = 500
 BULLET.Spread = 0
---BULLET.Gravity = 0.06
+--BULLET.Gravity = 0
 BULLET.AffectedBySBGravity = true
 
 -- Damage
-BULLET.DamageType = "BlastDamage"
-BULLET.Damage = 140
+BULLET.DamageType = "EMPDamage"
+BULLET.Damage = 700
 BULLET.Radius = 250
+BULLET.Duration = 10
 BULLET.RangeDamageMul = 2.2
 BULLET.PlayerDamage = 100
-BULLET.PlayerDamageRadius = 100
+BULLET.PlayerDamageRadius = 200
 
 -- Reload/Ammo
-BULLET.Reloadtime = 5.5
+BULLET.Reloadtime = 8
 BULLET.Ammo = 0
 BULLET.AmmoReloadtime = 0
 
