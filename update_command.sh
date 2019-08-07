87c13dc52c3045d481f55a1b51539eb22d874759
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-08-07 18:24:39 -0400

    Update update command

--- update_command.sh
+++ update_command.sh
@@ -1,6 +1,7 @@
 pewpew="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $pewpew
 
-git --git-dir=$pewpew/.git fetch --all
-git checkout -- $pewpew/
-git --git-dir=$pewpew/.git reset --hard origin/master
+cd $pewpew
+
+git fetch --all &&
+git checkout -- $pewpew &&
+git reset --hard origin/master
